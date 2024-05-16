ef10a325eecbc3c6c19576a171c878963705429d
Ver <aloss8411@gmail.com>
2024-05-16 21:01:31 +0800

    Initial Commit

---
+++ FirebaseDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseDemo
+//
+//  Created by Wei Ran Wang on 2024/5/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
