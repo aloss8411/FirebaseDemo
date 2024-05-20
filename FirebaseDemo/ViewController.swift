a99003dd5912f9e47ca25a1e4cfc6db5e6611fcc
Ver <aloss8411@gmail.com>
2024-05-20 12:20:15 +0800

    [Bug] fix minor bugs

--- FirebaseDemo/ViewController.swift
+++ FirebaseDemo/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        appendItem()
+        appendItem()
 //        loadItem()
     }
     
